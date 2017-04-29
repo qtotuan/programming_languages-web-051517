9998919df4826a133de14e8fd5587e5e889cbaf8
Quynh To Tuan <quynh.totuan@gmail.com>
2017-04-29 12:34:10 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
-  # your code here
+  hash = {}
+  languages.each do |top_class, languages_hash|
+    languages_hash.each do |language, type|
+      if hash[language] == nil
+        hash[language] = {:type => type[:type]}
+      end
+
+      if hash[language][:style] == nil
+        hash[language][:style] = [top_class]
+      else
+        hash[language][:style] << top_class
+      end
+    end
+  end
+  hash
 end
